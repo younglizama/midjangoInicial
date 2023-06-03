c185cf6b8bb15f9afa88664633e282439b019e7c
younglizama <co.lizama@duocuc.cl>
2023-06-03 10:48:18 -0400

    segunda carga

--- midjango/settings.py
+++ midjango/settings.py
@@ -74,10 +74,27 @@ WSGI_APPLICATION = 'midjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME':'127.0.0.1:1521/orcl',
+        'USER': 'c##prueba',
+        'PASSWORD': 'prueba',
+        'TEST':{
+            'USER': 'defaul_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBL_SPACE_TMP': 'defaul_test_tbls_tmp',
+    
+        },
+
+
     }
 }
 
